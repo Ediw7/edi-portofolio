1e58c237e371a72b0dd3ae23699f3dd0c99bd220
Ediw7 <ediwicoro26@gmail.com>
2025-06-12 22:35:16 +0700

    fix: responsive halaman semua

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,25 @@
-// App.js
-
 import React from 'react';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import About from './components/About';
-import Portofolio from './components/Portofolio';
+// Nama import dikembalikan agar sesuai dengan nama file Anda: Portofolio.jsx
+import Portofolio from './components/Portofolio'; 
 import Prestasi from './components/Prestasi';
 import Pengalaman from './components/Pengalaman';
 import Footer from './components/Footer';
 
 const App = () => {
   return (
-    <div>
+    <div className="overflow-x-hidden bg-gray-900">
       <Navbar /> 
-      <div>
-        <section id="home">
-          <Home />
-        </section>
-        <section id="about">
-          <About />
-        </section>
-        <section id="portfolio">
-          <Portofolio />
-        </section>
-        <section id="prestasi">
-          <Prestasi />
-        </section>
-        <section id="pengalaman">
-          <Pengalaman />
-        </section>
-      </div>
+      <main>
+        <Home />
+        <About />
+        {/* Nama komponen yang digunakan di JSX juga disesuaikan */}
+        <Portofolio />
+        <Prestasi />
+        <Pengalaman />
+      </main>
       <Footer />
     </div>
   );
